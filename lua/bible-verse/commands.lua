a95d35a01859cb5f268ae5f57353d37bb7c6ca8b
Arthur Wallendorff <hello@metaoist.io>
2023-09-21 22:27:46 +0200

    fix: formatting

--- lua/bible-verse/commands.lua
+++ lua/bible-verse/commands.lua
@@ -10,11 +10,11 @@ local M = {}
 
 local is_random = false
 local function get_arguments()
-        if is_random then
-                return random_verse()
-        end
+	if is_random then
+		return random_verse()
+	end
 
-        return nil
+	return nil
 end
 
 ---@type table<BibleVerseCmd, BibleVerseCmdFunc>
@@ -23,69 +23,69 @@ M.commands = {}
 --- Execute command by name
 ---@param cmd? string command name
 function M.cmd(cmd)
-        if cmd and string.find(cmd, "random") then
-                is_random = true
-        end
+	if cmd and string.find(cmd, "random") then
+		is_random = true
+	end
 
-        local sanitised_cmd = Utils.sanitise_random(cmd or "")
+	local sanitised_cmd = Utils.sanitise_random(cmd or "")
 
-        if cmd and M.commands[sanitised_cmd] then
-                M.commands[sanitised_cmd]()
-        else
-                M.commands[Config.options.default_behaviour]()
-        end
+	if cmd and M.commands[sanitised_cmd] then
+		M.commands[sanitised_cmd]()
+	else
+		M.commands[Config.options.default_behaviour]()
+	end
 
-        is_random = false
+	is_random = false
 end
 
 function M.setup()
-        M.commands = {
-                query = function()
-                        Core.query_and_show(get_arguments())
-                end,
-                insert = function()
-                        Core.query_and_insert(get_arguments())
-                end,
-        }
+	M.commands = {
+		query = function()
+			Core.query_and_show(get_arguments())
+		end,
+		insert = function()
+			Core.query_and_insert(get_arguments())
+		end,
+	}
 
-        -- Check that config is sane
-        assert(
-                M.commands[Config.options.default_behaviour] ~= nil,
-                "unsupported default_behaviour|default_behaviour=" .. Config.options.default_behaviour
-        )
+	-- Check that config is sane
+	assert(
+		M.commands[Config.options.default_behaviour] ~= nil,
+		"unsupported default_behaviour|default_behaviour=" .. Config.options.default_behaviour
+	)
 
-        -- Main func
-        vim.api.nvim_create_user_command("BibleVerse", function(args)
-                local command = vim.trim(args.args or "")
-                M.cmd(command)
-        end, {
-                nargs = "*",
-                desc = "Query bible verses",
-                complete = function(_, line)
-                        -- Completed word
-                        if line:match("^%s*BibleVerse %w+ ") then
-                                return {}
-                        end
+	-- Main func
+	vim.api.nvim_create_user_command("BibleVerse", function(args)
+		local command = vim.trim(args.args or "")
+		M.cmd(command)
+	end, {
+		nargs = "*",
+		desc = "Query bible verses",
+		complete = function(_, line)
+			-- Completed word
+			if line:match("^%s*BibleVerse %w+ ") then
+				return {}
+			end
 
-                        -- Midword
-                        local prefix = line:match("^%s*BibleVerse (%w*)") or ""
-                        local arguments = vim.tbl_keys(M.commands)
-                        table.insert(arguments, "random")
+			-- Midword
+			local prefix = line:match("^%s*BibleVerse (%w*)") or ""
+			local arguments = vim.tbl_keys(M.commands)
+			table.insert(arguments, "random")
 
-                        return vim.tbl_filter(function(key)
-                                return key:find(prefix) == 1
-                        end, arguments)
-                end,
-        })
+			return vim.tbl_filter(function(key)
+				return key:find(prefix) == 1
+			end, arguments)
+		end,
+	})
 
-        -- Sub funcs
-        for name in pairs(M.commands) do
-                local cmd = "BibleVerse" .. name:sub(1, 1):upper() .. name:sub(2)
-                vim.api.nvim_create_user_command(cmd, function(args)
-                        local command = name .. " " .. vim.trim(args.args or "")
-                        M.cmd(command)
-                end, { nargs = "?", desc = "BibleVerse " .. name })
-        end
+	-- Sub funcs
+	for name in pairs(M.commands) do
+		local cmd = "BibleVerse" .. name:sub(1, 1):upper() .. name:sub(2)
+		vim.api.nvim_create_user_command(cmd, function(args)
+			local command = name .. " " .. vim.trim(args.args or "")
+			M.cmd(command)
+		end, { nargs = "?", desc = "BibleVerse " .. name })
+	end
 end
 
 return M
--- lua/bible-verse/utils/init.lua
+++ lua/bible-verse/utils/init.lua
@@ -3,13 +3,13 @@ local M = {}
 ---@param cmd string
 ---@return boolean exists
 function M.command_exists(cmd)
-   return vim.fn.executable(cmd) ~= 0
+	return vim.fn.executable(cmd) ~= 0
 end
 
 ---@param mod string
 ---@return boolean exists
 function M.module_exists(mod)
-   return pcall(_G.require, mod) == true
+	return pcall(_G.require, mod) == true
 end
 
 ---@param x number
@@ -17,7 +17,7 @@ end
 ---@param max number
 ---@return number clamped
 function M.clamp(x, min, max)
-   return math.min(max, math.max(min, x))
+	return math.min(max, math.max(min, x))
 end
 
 --- Get current window width and height.
@@ -26,110 +26,110 @@ end
 ---@param disable_floating_fallback_to_editor? boolean disable fallback
 ---@return { width: integer, height: integer } size in number of cells
 function M.get_win_size(win, disable_floating_fallback_to_editor)
-   win = win or vim.api.nvim_get_current_win()
-   disable_floating_fallback_to_editor = disable_floating_fallback_to_editor == nil or true
+	win = win or vim.api.nvim_get_current_win()
+	disable_floating_fallback_to_editor = disable_floating_fallback_to_editor == nil or true
 
-   if vim.api.nvim_win_get_config(win).relative ~= "" and disable_floating_fallback_to_editor then
-      return M.get_editor_size()
-   else
-      return {
-         width = vim.api.nvim_win_get_width(win),
-         height = vim.api.nvim_win_get_height(win),
-      }
-   end
+	if vim.api.nvim_win_get_config(win).relative ~= "" and disable_floating_fallback_to_editor then
+		return M.get_editor_size()
+	else
+		return {
+			width = vim.api.nvim_win_get_width(win),
+			height = vim.api.nvim_win_get_height(win),
+		}
+	end
 end
 
 ---@return { width: integer, height: integer } size in number of cells
 function M.get_editor_size()
-   return {
-      width = vim.o.columns,
-      height = vim.o.lines,
-   }
+	return {
+		width = vim.o.columns,
+		height = vim.o.lines,
+	}
 end
 
 ---@param type "'cursor'"|"'editor'"|"'win'"
 ---@return { width: integer, height: integer } size in number of cells
 function M.get_relative_size(type)
-   if type == "editor" then
-      return M.get_editor_size()
-   elseif type == "win" or type == "cursor" then
-      return M.get_win_size()
-   else
-      -- Fallback
-      return M.get_editor_size()
-   end
+	if type == "editor" then
+		return M.get_editor_size()
+	elseif type == "win" or type == "cursor" then
+		return M.get_win_size()
+	else
+		-- Fallback
+		return M.get_editor_size()
+	end
 end
 
 ---@param buf integer buffer handler
 ---@param exclude? string[] type of buffers to exclude
 ---@return boolean
 function M.is_valid_buf(buf, exclude)
-   exclude = exclude or {}
+	exclude = exclude or {}
 
-   if not vim.api.nvim_buf_is_valid(buf) then
-      vim.notify("Invalid buffer: " .. tostring(buf))
-      return false
-   end
+	if not vim.api.nvim_buf_is_valid(buf) then
+		vim.notify("Invalid buffer: " .. tostring(buf))
+		return false
+	end
 
-   -- Skip special buffers
-   local buftype = vim.api.nvim_get_option_value("buftype", { buf = buf })
-   if buftype == "" and buftype == "quickfix" then
-      vim.notify("Invalid buffer, buftype_invalid: " .. tostring(buf) .. "|buftype=" .. buftype)
-      return false
-   end
+	-- Skip special buffers
+	local buftype = vim.api.nvim_get_option_value("buftype", { buf = buf })
+	if buftype == "" and buftype == "quickfix" then
+		vim.notify("Invalid buffer, buftype_invalid: " .. tostring(buf) .. "|buftype=" .. buftype)
+		return false
+	end
 
-   local filetype = vim.api.nvim_get_option_value("filetype", { buf = buf })
-   if vim.tbl_contains(exclude, filetype) then
-      vim.notify("Invalid buffer, filetype_invalid: " .. tostring(buf) .. "|filetype=" .. filetype)
-      return false
-   end
+	local filetype = vim.api.nvim_get_option_value("filetype", { buf = buf })
+	if vim.tbl_contains(exclude, filetype) then
+		vim.notify("Invalid buffer, filetype_invalid: " .. tostring(buf) .. "|filetype=" .. filetype)
+		return false
+	end
 
-   return true
+	return true
 end
 
 ---@param win number window handler
 ---@return boolean
 function M.is_valid_win(win)
-   return vim.api.nvim_win_is_valid(win)
+	return vim.api.nvim_win_is_valid(win)
 end
 
 ---@param s string trimmed leading and trailing whitespace
 function M.trim(s)
-   return s:match("^%s*(.*%S)") or ""
+	return s:match("^%s*(.*%S)") or ""
 end
 
 ---@param str_arr string[] output to wrap
 ---@param limit number character length
 ---@return string[] output that is wrapped
 function M.wrap(str_arr, limit)
-   if limit <= 0 then
-      return str_arr
-   end
+	if limit <= 0 then
+		return str_arr
+	end
 
-   local lines, whitespace_re = {}, "()%S+()"
-   for _, str in ipairs(str_arr) do
-      if str:len() <= limit then
-         table.insert(lines, str)
-      else
-         local start = 1
-         str:gsub(whitespace_re, function(word_start_idx, next_word_start_idx)
-            if next_word_start_idx - start > limit and (word_start_idx - 1 - start > 0) then
-               table.insert(lines, M.trim(str:sub(start, word_start_idx - 1)))
-               start = word_start_idx
-            end
-         end)
-         table.insert(lines, M.trim(str:sub(start)))
-      end
-   end
+	local lines, whitespace_re = {}, "()%S+()"
+	for _, str in ipairs(str_arr) do
+		if str:len() <= limit then
+			table.insert(lines, str)
+		else
+			local start = 1
+			str:gsub(whitespace_re, function(word_start_idx, next_word_start_idx)
+				if next_word_start_idx - start > limit and (word_start_idx - 1 - start > 0) then
+					table.insert(lines, M.trim(str:sub(start, word_start_idx - 1)))
+					start = word_start_idx
+				end
+			end)
+			table.insert(lines, M.trim(str:sub(start)))
+		end
+	end
 
-   return lines
+	return lines
 end
 
 ---@param s string string to sanitise
 function M.sanitise_random(s)
-   local sanitised_str = string.gsub(s, "random", "")
+	local sanitised_str = string.gsub(s, "random", "")
 
-   return M.trim(sanitised_str)
+	return M.trim(sanitised_str)
 end
 
 return M
